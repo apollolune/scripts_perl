6c5c39671505b81ebf0cd557c1bdfaef5a5a73bb
apol <apollolune@gmail.com>
2012-02-09 15:22:29 +0100

    sendmail.pl : add

---
+++ sendmail.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+## !/opt/local/bin/perl -w
+use strict;
+use Carp;
+
+use LWP::Simple;
+use MIME::Lite;
+
+# my $url = "abc.org/?xyz=csv";
+# my $file = "/Users/abc/Desktop/abc.csv";
+
+# my $status = mirror($url,$file);
+
+# die "Cannot retrieve $url" unless is_success($status);
+if ( !$ARGV[0] ) {
+    print "Il faut un message à envoyer par mail.\n";
+    exit(1);
+}
+
+my $myhost = `hostname`;
+chomp $myhost;
+my @message = @ARGV;
+my $message = join(" ", @message);
+
+my $msg;
+$msg = MIME::Lite->new(
+    From    => 'apollolune@gmail.com',
+    To      => 'marc.louis.perso@gmail.com',
+    Subject => "Task done on $myhost",
+	Data     => $message,
+);
+
+# $msg->attach(
+		# Type	 => 'Text',
+		# Data     => "Essai d'envoi de mail d'un script Perl.",
+# );
+# $msg->attach(
+		# Type	 => 'Application/Text',
+		# Path     => "/Users/abc/Desktop/abc.csv"
+# );
+
+$msg->send;
